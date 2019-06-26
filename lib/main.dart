836e8ccec087ab0bbdba551634e33548c9435e97
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-26 18:41:10 +0300

    import HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:moor_sqlite/data/moor_database.dart';
+import 'package:moor_sqlite/ui/home_page.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
